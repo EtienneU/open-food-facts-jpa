60e3d26ffbc79e361c34e58a980b176c4fd5d4f8
EtienneUrbano <urbano.etienne@laposte.net>
2021-02-25 11:04:43 +0100

    First Commit - connexion avec le projet GitHub distant

---
+++ src/fr/diginamic/openfoodfact/IntegrationOpenFoodFacts.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.diginamic.openfoodfact;
+
+/**
+ * @author EtienneUrbano
+ *
+ */
+public class IntegrationOpenFoodFacts {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
